10670adcc2948eaf3b5974096373a2bb4811b1ec
Keziah Biermann <development@kizzycode.de>
2023-03-20 17:31:35 +0100

    Update template

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+#![doc = include_str!("../README.md")]
+// Clippy lints
+#![warn(clippy::large_stack_arrays)]
+#![warn(clippy::arithmetic_side_effects)]
+#![warn(clippy::expect_used)]
+#![warn(clippy::unwrap_used)]
+#![warn(clippy::indexing_slicing)]
+#![warn(clippy::panic)]
+#![warn(clippy::todo)]
+#![warn(clippy::unimplemented)]
+#![warn(clippy::unreachable)]
+#![warn(clippy::missing_panics_doc)]
+#![warn(clippy::allow_attributes_without_reason)]
+#![warn(clippy::cognitive_complexity)]
+
 #[cfg(test)]
 mod tests {
     #[test]
